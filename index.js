935d4070513763f449473307d1840d30c57c5bff
Afrikhana <brianmurigi64@gmail.com>
2023-03-26 21:19:16 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (param1, param2) => {
+    console.log(`Adding ${param1}`);
+    console.log(`Adding ${param2}`);
+    return param1 / param2;
+  }
+  divide(2000,100);
+
+
+const square = (x) => x*x;
+square(2);
+
+const add= (x,y) => x+y;
+add(3,4);
